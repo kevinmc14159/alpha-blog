76d64a4e06e721dfbaf5ed12ef2b30445b43c68f
Kevin Chang <kevinchang14159@gmail.com>
2016-12-28 11:55:29 -0800

    Minor styling fixes

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ActiveRecord::Base
 
   # Forces articles to have a title & description of specified length
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 1_000_000 }
 
 end
