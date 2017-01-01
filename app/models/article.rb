fb270ec69e3575b3f89eb473c622886fee046b9d
Kevin Chang <kevinchang14159@gmail.com>
2017-01-01 11:05:32 -0800

    Added article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
   # Establish Active Record Association
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
 
   # Forces articles to have a title & description of specified length
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
+  # Establish Active Record Association
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
 end
